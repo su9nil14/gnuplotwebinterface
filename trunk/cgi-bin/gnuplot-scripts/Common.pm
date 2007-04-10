0df5961bc35f3ca68c9f1c4295af890f0ed0af59
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-10 17:43:13 +0000

    gnuplot, stop using single quotes for non-interpolation

--- trunk/cgi-bin/gnuplot-scripts/Common.pm
+++ trunk/cgi-bin/gnuplot-scripts/Common.pm
@@ -23,9 +23,9 @@ sub gnuplot_math_settings {
 }
 
 sub gnuplot_timeseries_settings {
-  return 'set xdata time  	# The x axis data is time
-  set timefmt "%d-%b-%y"	# The dates in the file look like 10-Jun-04
-  set format x "%b %d"	# On the x-axis, we want tics like Jun 10'
+  return "set xdata time  	# The x axis data is time
+  set timefmt \"%d-%b-%y\"	# The dates in the file look like 10-Jun-04
+  set format x \"%b %d\""	# On the x-axis, we want tics like Jun 10'
 }
 
 
