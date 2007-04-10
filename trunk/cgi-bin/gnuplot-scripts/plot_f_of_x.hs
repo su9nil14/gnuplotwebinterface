a56cbc04d8ceb5e5d8f62ea0da40e256fe1fb8c4
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-10 17:01:16 +0000

    gnuplot cgi

--- trunk/cgi-bin/gnuplot-scripts/plot_f_of_x.hs
+++ trunk/cgi-bin/gnuplot-scripts/plot_f_of_x.hs
@@ -1,3 +1,14 @@
 #!/usr/lib/ghc-6.6/bin/runghc
+import System
+import Common ( blee )
 
-main = print "blee"
+main = do args <- getArgs
+          let usagemsg = "usage example: $ runghc plot_f_of_x.hs math-2d 'sin (x)'"
+                         
+          case args of
+            [style,function] -> gen_gnuplot_script style function
+            _                    -> error $ "arg count does not equal 2. args: : " ++ ( show args ) ++ "\n" ++ usagemsg
+
+gen_gnuplot_script style function = let blee = "blah"
+                                      in do putStrLn $ unlines [style, function]
+                                            putStrLn blee
