ba70b3edcce9384ab8b121262a1a1a8baed42a77
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-11 12:36:53 +0000

    prep for porting gnuplot financial script to haskell

---
+++ trunk/cgi-bin/gnuplot-scripts/financial.sh
@@ -0,0 +1,17 @@
+echo ./plot_f_of_x.pl 'math-2d' 'sin (x)'
+./plot_f_of_x.pl 'math-2d' 'sin (x)'
+echo "***********"
+
+echo ./plot_f_of_x.hs 'math-2d' 'sin (x)'
+echo 
+./plot_f_of_x.hs 'math-2d' 'sin (x)'
+echo "***********"
+
+echo ./plot_f_of_x.hs 'math-3d' 'sin (x)'
+echo
+./plot_f_of_x.hs 'math-3d' 'sin (x)'
+echo "***********"
+
+echo ./plot_f_of_x.hs 'math-4d' 'sin (x)'
+echo
+./plot_f_of_x.hs 'math-4d' 'sin (x)'
