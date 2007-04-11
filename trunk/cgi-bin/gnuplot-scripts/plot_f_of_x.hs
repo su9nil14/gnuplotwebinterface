0c4a0725bc1ebe2e2a94392bc7a79e6a8ae87cf7
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-11 12:31:05 +0000

    gnuplot cgi, perl to haskell -- looks pretty good to me.

--- trunk/cgi-bin/gnuplot-scripts/plot_f_of_x.hs
+++ trunk/cgi-bin/gnuplot-scripts/plot_f_of_x.hs
@@ -1,5 +1,4 @@
 #!/usr/lib/ghc-6.6/bin/runghc 
-{-# OPTIONS_GHC -fglasgow-exts #-} 
 import System
 import Common 
 
@@ -8,11 +7,8 @@ main = do args <- getArgs
                          "                $ runghc plot_f_of_x.hs math-3d 'sin (x)'"
           case args of
             [style,function] -> gen_gnuplot_script style function
-            _                    -> error $ "arg count does not equal 2. args: : " ++ ( show args ) ++ "\n" ++ usagemsg
+            _                -> error $ "arg count does not equal 2. args: : " ++ ( show args ) ++ "\n" ++ usagemsg
 
-style_to_plotcmd =  [("math-2d","plot"),("math-3d","splot")]
 
-gen_gnuplot_script style function | Just plotcmd
-                                      <- lookup style style_to_plotcmd 
-                                      = putStrLn $ gnuplot_math_settings ++ "\n" ++ plotcmd ++ " "  ++ function
-                                  | otherwise = error $ "bad style: " ++ style
+
+
