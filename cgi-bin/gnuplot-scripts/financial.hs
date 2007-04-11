73de83e3d3d24fd19e95a8204d353875495066d4
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-11 12:36:53 +0000

    prep for porting gnuplot financial script to haskell

---
+++ cgi-bin/gnuplot-scripts/financial.hs
@@ -0,0 +1,14 @@
+#!/usr/lib/ghc-6.6/bin/runghc 
+import System
+import Common 
+
+main = do args <- getArgs
+          let usagemsg = "usage examples: $ runghc plot_f_of_x.hs math-2d 'sin (x)'\n" ++
+                         "                $ runghc plot_f_of_x.hs math-3d 'sin (x)'"
+          case args of
+            [style,function] -> gen_gnuplot_script style function
+            _                -> error $ "bad arguments: " ++ ( show args ) ++ "\n" ++ usagemsg
+
+
+
+
