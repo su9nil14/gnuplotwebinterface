4737bab17d4abce3909e7348b8419ce6d02580e8
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-12 10:37:43 +0000

    added type sigs

--- trunk/cgi-bin/gnuplot-scripts/Common.hs
+++ trunk/cgi-bin/gnuplot-scripts/Common.hs
@@ -13,12 +13,15 @@ gnuplot_timeseries_settings = gnuplot_png_settings ++ "\n" ++
                               "set timefmt \"%d-%b-%y\" # The dates in the file look like 10-Jun-04 \n" ++
                               "set format x \"%b %d\"   #On the x-axis, we want tics like Jun 10"
 
+
+gen_gnuplot_math_script :: String -> String -> IO ()
 gen_gnuplot_math_script style function = let maybePlotCmd = lookup style style_to_plotcmd 
                                              style_to_plotcmd =  [("math-2d","plot"),("math-3d","splot")]
                                            in case maybePlotCmd of
                                                 Just plotcmd -> putStrLn $ gnuplot_math_settings ++ "\n" ++ plotcmd ++ " "  ++ function
                                                 _            -> error $ "bad style: " ++ style
 
+gen_gnuplot_financial_script :: String -> String -> String -> String -> IO ()
 gen_gnuplot_financial_script company displaymode startDate endDate
     = let maybeCompanyFile = lookup company     company_to_companyfile 
           maybeModeString  = lookup displaymode displaymode_to_modestring
