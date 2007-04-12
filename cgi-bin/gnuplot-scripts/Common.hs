7ec9dea2a1fe2336acd8a9bd4fd6fe1b02cbe458
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-12 10:04:47 +0000

    gnuplot, gen financial haskell seems pretty good

--- cgi-bin/gnuplot-scripts/Common.hs
+++ cgi-bin/gnuplot-scripts/Common.hs
@@ -1,16 +1,17 @@
 module Common where
 
-gnuplot_png_settings = "set terminal png transparent nocrop enhanced size 600,400\n\
-                        \  set pm3d implicit at s"
+gnuplot_png_settings = "set terminal png transparent nocrop enhanced size 600,400\n" ++
+                       "set pm3d implicit at s"
 
 gnuplot_math_settings =  gnuplot_png_settings ++ "\n" ++
                          "set border 4095 \n\
                          \  set xlabel \"x\" \n\
                          \  set ylabel \"y\""
 
-gnuplot_timeseries_settings = "set xdata time\n" ++  	                -- The x axis data is time
-                              "  set timefmt \"%d-%b-%y\" \n" ++	-- The dates in the file look like 10-Jun-04
-                              "  set format x \"%b %d\""	        -- On the x-axis, we want tics like Jun 10'
+gnuplot_timeseries_settings = gnuplot_png_settings ++ "\n" ++
+                              "set xdata time\n" ++  	                -- The x axis data is time
+                              "set timefmt \"%d-%b-%y\" \n" ++	-- The dates in the file look like 10-Jun-04
+                              "set format x \"%b %d\""	        -- On the x-axis, we want tics like Jun 10'
 
 
 gen_gnuplot_math_script style function = let maybePlotCmd = lookup style style_to_plotcmd 
@@ -20,12 +21,23 @@ gen_gnuplot_math_script style function = let maybePlotCmd = lookup style style_t
                                                 _            -> error $ "bad style: " ++ style
 
 gen_gnuplot_financial_script company displaymode startDate endDate
-    = let maybeCompanyFile = lookup company company_to_companyfile 
-          maybeModeString = lookup displaymode displaymode_to_modestring
-          company_to_companyfile =  [("math-2d","plot"),("math-3d","splot")]
-          displaymode_to_modestring = [("",""),("","")]
-        in case ( maybeCompanyFile, maybeModeString ) of
-             ( Just companyfile, Just displaymodestring )  -> putStrLn $ gnuplot_math_settings ++ "\n" ++
-                                                                         "plot "  
-             _            -> error $ "bad lookup. " ++ company ++     " -> company file: " ++ ( show maybeCompanyFile ) ++ "\n" ++
-                                     "            " ++ displaymode ++ " -> displaymode: "  ++ ( show maybeModeString ) 
+    = let maybeCompanyFile = lookup company     company_to_companyfile 
+          maybeModeString  = lookup displaymode displaymode_to_modestring
+          maybeTitleEnd    = lookup displaymode displaymode_to_titleend 
+          company_to_companyfile =  [("ibm","data/ibm.dat"),("cisco","data/cisco.dat")]
+          displaymode_to_modestring = [("points", "using 1:2 with linespoints"),
+                                       ("candles","using 1:($2+$3+$4+$5)/4:4:3 with yerrorbars")]
+          displaymode_to_titleend = [("points","daily prices"),("candles","opening prices")]
+        in case ( maybeCompanyFile,
+                  maybeModeString,
+                  maybeTitleEnd ) of
+                ( Just companyfile,
+                  Just modestring,
+                  Just titleEnd)
+                  -> putStrLn $ gnuplot_timeseries_settings ++ "\n" ++
+                              "plot [\"" ++ startDate ++ "\":\"" ++ endDate ++ "\"]"
+                              ++ " '" ++ companyfile ++ "'"
+                              ++ modestring
+                              ++ " title \" " ++ company ++ " " ++ titleEnd ++ "\""
+                _ -> error $ "bad lookup. " ++ company ++     " -> company file: " ++ ( show maybeCompanyFile ) ++ "\n" ++
+                             "            " ++ displaymode ++ " -> displaymode: "  ++ ( show maybeModeString ) 
--- cgi-bin/gnuplot-scripts/financial.sh
+++ cgi-bin/gnuplot-scripts/financial.sh
@@ -1,4 +1,3 @@
-echo ./plot_f_of_x.pl ibm points 31-May-04 11-Jun-04 
 echo ./financial.pl ibm points 31-May-04 11-Jun-04 
 ./financial.pl ibm points 31-May-04 11-Jun-04 
 
@@ -8,6 +7,9 @@ echo "***********"
 echo ./financial.hs ibm points 31-May-04 11-Jun-04 
 ./financial.hs ibm points 31-May-04 11-Jun-04 
 echo "***********"
+
+echo ./financial.pl cisco candles 31-May-04 11-Jun-04 
+./financial.pl cisco candles 31-May-04 11-Jun-04 
 
 echo ./financial.hs cisco candles 31-May-04 11-Jun-04 
 ./financial.hs cisco candles 31-May-04 11-Jun-04 
