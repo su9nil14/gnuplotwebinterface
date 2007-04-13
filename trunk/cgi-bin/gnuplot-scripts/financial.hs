c6f0ec5efd434f846cc6f97d263c94a6dcf8ad0e
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-13 11:55:55 +0000

    indentation

--- trunk/cgi-bin/gnuplot-scripts/financial.hs
+++ trunk/cgi-bin/gnuplot-scripts/financial.hs
@@ -7,8 +7,9 @@ main = do args <- getArgs
                          "                $ runghc ./financial.hs cisco points  31-May-04 11-Jun-04 \n" ++
                          "                $ runghc ./financial.hs cisco candles 31-May-04 11-Jun-04"
           case args of
-            [company, displaymode, startDate, endDate] -> gen_gnuplot_financial_script company displaymode startDate endDate
-            _                -> error $ "bad arguments: " ++ ( show args ) ++ "\n" ++ usagemsg
+            [company, displaymode, startDate, endDate]
+              -> gen_gnuplot_financial_script company displaymode startDate endDate
+            _ -> error $ "bad arguments: " ++ ( show args ) ++ "\n" ++ usagemsg
 
 
 
