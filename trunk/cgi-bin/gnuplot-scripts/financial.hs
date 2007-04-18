99b2ef2ba9d6d9f000fee293e2253e1991de68c4
thomashartman1 <thomashartman1@98e4ef2a-a82c-0410-87f7-27a8c8063a5b>
2007-04-18 12:53:51 +0000

    incorporated Jeff and Claus's suggestions.

--- trunk/cgi-bin/gnuplot-scripts/financial.hs
+++ trunk/cgi-bin/gnuplot-scripts/financial.hs
@@ -1,38 +1,36 @@
 #!/usr/lib/ghc-6.6/bin/runghc 
 import System
 import Common 
+import Control.Monad.Error
 
 main = do args <- getArgs
           case args of
             [company, displaymode, startDate, endDate]
-              -> financial_output_wrapper company displaymode startDate endDate
+              -> putStrLn $ financial_output company displaymode startDate endDate
             _ -> error $ "bad arguments: " ++ ( show args ) ++ "\n" ++ usagemsg
-
-
-
-financial_output_wrapper :: String -> String -> String -> String -> IO ()
-financial_output_wrapper company displaymode startDate endDate =
-  do
-    let  maybeCompanyFile = lookup company company_to_companyfile
-    validate_arg "company" company maybeCompanyFile usagemsg
-
-    let  maybeModeString  = lookup displaymode displaymode_to_modestring
-    validate_arg "display mode" displaymode maybeModeString usagemsg
-
-
-    let  maybeTitleEnd = lookup displaymode displaymode_to_titleend 
-    validate_arg "title end" displaymode maybeTitleEnd usagemsg
-
-    let maybeScript  = gen_gnuplot_financial_script company
-                                                    ( maybeCompanyFile  )
-                                                    ( maybeModeString )
-                                                    ( maybeTitleEnd )
-                                                    startDate endDate
-    case maybeScript of 
-             Just script -> putStrLn script
-             _           -> error $ "bad script" ++ "\n" ++ usagemsg
-
 
 usagemsg = "usage examples: $ runghc ./financial.hs ibm   points  31-May-04 11-Jun-04 \n" ++
            "                $ runghc ./financial.hs cisco points  31-May-04 11-Jun-04 \n" ++
            "                $ runghc ./financial.hs cisco candles 31-May-04 11-Jun-04"
+
+
+financial_output :: String -> String -> String -> String -> String
+financial_output company displaymode startDate endDate = financial_script
+ where
+ financial_script = gnuplot_timeseries_settings ++ "\n"
+                 ++ "plot [\"" ++ startDate ++ "\":\"" ++ endDate ++ "\"]"
+                 ++ " '" ++ companyFile ++ "'" ++ modeString
+                 ++ " title \"" ++ company ++ " " ++ titleEnd ++ "\""
+
+ companyFile = lookupWith (error $ "no company file for " ++ company)
+                 company company_to_companyfile
+
+ modeString  = lookupWith (error $ "no mode string for " ++ displaymode)
+                 displaymode displaymode_to_modestring
+
+ titleEnd    = lookupWith (error $ "no title end for " ++ displaymode)
+                 displaymode displaymode_to_titleend
+
+ lookupWith error key assocs = maybe error id $ lookup key assocs
+
+
